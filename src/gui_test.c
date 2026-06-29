5b6c58aa8a8d3eb172bde583558ce1b25b66bcea
KingOfHearts40 <AceofHearts40@outlook.com>
2026-06-29 05:29:43 -0400

    viewport resizing and dragging bool added to fix frame dependence of movement if the mouse was moved too quickly. added min width/height to the function

--- src/gui_test.c
+++ src/gui_test.c
@@ -11,6 +11,8 @@ typedef struct viewport_data{
     Rectangle bounds;
     Color color;
     Rectangle resize_toggle;
+    bool resizing;
+    bool dragging;
 }viewport_data;
 
 viewport_data * viewports = NULL;
@@ -26,28 +28,6 @@ void logError(char * file, char * error_message){
         printf("could not open file %s", file);
     }
     fclose(fptr);
-}
-
-void addViewPortMemory(viewport_data **v){
-    char *log_file = "../save_data/log_file.txt";
-    if (*v == NULL){
-        *v = malloc(sizeof(viewport_data) * 4);
-            if (*v == NULL){
-            logError(log_file,"viewport malloc failed\n");  
-            }
-            else viewport_capacity= 4;
-    }
-
-    else if(viewport_length == viewport_capacity && viewport_length > 0){
-        void * temp_ptr = realloc(*v, sizeof(viewport_data) * viewport_capacity * 2);
-        if(temp_ptr){
-            *v = temp_ptr;
-            viewport_capacity *= 2;
-        }
-        else{
-            logError(log_file, "realloc for view_port failed");
-        }
-    }
 }
 
 //this can be used to allocate memory for any ptr that has a tracking 
@@ -99,6 +79,7 @@ void createNewViewPort(){
 
     viewports[viewport_length].bounds = (Rectangle){.height = 100, .width = 100, .x = 500, .y = 100};
     viewports[viewport_length].color = BLACK;
+    viewports[viewport_length].resizing = false;
 
     viewports[viewport_length].resize_toggle = (Rectangle){.height = 20, .width = 20, 
         .x = viewports[viewport_length].bounds.x +viewports[viewport_length].bounds.width - 20, 
@@ -114,22 +95,57 @@ void drawViewPorts(){
     }
 }
 
-void updateViewportsPositions(Vector2 delta){
+void toggleViewportDragResize(){
+    bool collided_rec = false;
     for(int i = 0; i < viewport_length; i++){
+        if(collided_rec == true){
+            //hit a different rec, just want to set this one to false
+            viewports[i].resizing = false;
+            viewports[i].dragging = false;
+            continue; //go to next viewport to set it false            
+        }
+        //first check if I hit the viewport, then check for the toggle
         if(CheckCollisionPointRec(GetMousePosition(), viewports[i].bounds)){
-            if(!CheckCollisionPointRec(GetMousePosition(), viewports[i].resize_toggle)){
+            if(CheckCollisionPointRec(GetMousePosition(), viewports[i].resize_toggle)){
+                viewports[i].resizing = true;
+                collided_rec = true;
+            }
+            else{//didn't hit the toggle, but still hit the viewport, can drag
+                viewports[i].resizing = false;
+                viewports[i].dragging = true;
+                collided_rec = true;                
+            }
+        }
+        else{//didn't hit this viewPort, don't want to do anything
+            viewports[i].resizing = false;
+            viewports[i].dragging = false;
+        }        
+    }    
+}
+
+void updateViewportsPositions(Vector2 delta){
+    float min_width = 30.0f, min_height = 30.0f;
+
+    for(int i = 0; i < viewport_length; i++){
+        if(viewports[i].resizing == true){
+            //first make sure we don't get below the min width/height
+            if(viewports[i].bounds.width + delta.x <= min_width){
+                continue;
+            }
+            if(viewports[i].bounds.height + delta.y <= min_height){
+                continue;
+            }            
+            viewports[i].bounds.width += delta.x;
+            viewports[i].bounds.height += delta.y;
+            viewports[i].resize_toggle.x += delta.x;
+            viewports[i].resize_toggle.y += delta.y;             
+        }
+        else if(viewports[i].dragging == true){
                 viewports[i].bounds.x += delta.x;
                 viewports[i].bounds.y += delta.y;
-                viewports[i].resize_toggle.x += GetMouseDelta().x;
-                viewports[i].resize_toggle.y += GetMouseDelta().y;    
+                viewports[i].resize_toggle.x += delta.x;
+                viewports[i].resize_toggle.y += delta.y;    
             break; //if two overlap, this prevents moving them at the same time
-            }
-            else{
-                viewports[i].bounds.width += GetMouseDelta().x;
-                viewports[i].bounds.height += GetMouseDelta().y;
-                viewports[i].resize_toggle.x += GetMouseDelta().x;
-                viewports[i].resize_toggle.y += GetMouseDelta().y;                
-            }
         }
     }
 }
@@ -164,6 +180,10 @@ int gui_main(){
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), button_rec)){
             button_rec.x += GetMouseDelta().x;
             button_rec.y += GetMouseDelta().y;
+        }
+
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            toggleViewportDragResize();
         }
 
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
